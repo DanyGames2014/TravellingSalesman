087351f33395404217aa52f3b11c6ffdede88de5
Daniel Fildán <fildand@centrum.cz>
2023-02-26 21:20:21 +0100

    Add files via upload

---
+++ Permutations/Program.cs
@@ -0,0 +1,85 @@
+﻿using Permutations;
+
+namespace PermutationTesting
+{
+    internal struct Program
+    {
+        static void Main(string[] args)
+        {
+            // Search Parameters
+            int startingCity = 1;
+            int[] cities = new int[] { };
+            Route[] routes = new Route[] { };
+
+            // Temp Lists to insert stuff read from config file
+            List<int> citiesList = new List<int>();
+            List<Route> routesList = new List<Route>();
+
+            // Read Config File
+            string inputFilePath = (args.Length>0 ? args[0] : null) ?? "input.csv";
+            string outputFilePath = (args.Length > 1 ? args[1] : null) ?? "output.txt";
+            string[] a = File.ReadAllLines(inputFilePath);
+
+            // Parse Config File
+            for (int i = 0; i < a.Length; i++)
+            {
+                // Line 1 - Starting City
+                if(i == 0)
+                {
+                    startingCity = Convert.ToInt32(a[i]);
+                }
+
+                // Line 2 - Colon Separated City Numbers
+                else if(i == 1)
+                {
+                    var citiesLine = a[i].Split(",");
+                    foreach (var item in citiesLine)
+                    {
+                        int tempCity = Convert.ToInt32(item);
+                        if(!(tempCity == startingCity))
+                        {
+                            citiesList.Add(tempCity);
+                        }
+                    }
+
+                }
+
+                // Lines 3+ - Routes
+                else
+                {
+                    var lineSplit = a[i].Split(",");
+                    if(lineSplit.Length == 3)
+                    {
+                        Route route = new Route(
+                            Convert.ToInt32(lineSplit[0]),
+                            Convert.ToInt32(lineSplit[1]),
+                            Convert.ToInt32(lineSplit[2])
+                        );
+
+                        routesList.Add(route);
+                    }
+                }
+            }
+
+            // Convert Lists to Arrays for Performance when they will be heavily accessed
+            cities = citiesList.ToArray();
+            routes = routesList.ToArray();
+            
+
+            // Work Manager
+            WorkManager workManager = new(cities,routes, startingCity);
+
+            WorkerThread workerThread = new WorkerThread(workManager);
+            workerThread.Start();
+
+            // When all the work is done, display a promp to press any key instead of exiting
+            Console.WriteLine("\n" + workManager.bestResult);
+            StreamWriter outputFile = File.CreateText(outputFilePath);
+            outputFile.WriteLine(workManager.bestResult.ToString());
+            outputFile.Flush();
+            outputFile.Dispose();
+            Console.WriteLine("DONE! Press Any Key to Continue");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Permutations/WorkManager.cs
@@ -0,0 +1,128 @@
+﻿using Combinatorics.Collections;
+
+namespace Permutations
+{
+    internal class WorkManager
+    {
+        /// <summary>
+        /// ints representing cities
+        /// </summary>
+        public int[] cities;
+
+        /// <summary>
+        /// City to start in
+        /// </summary>
+        public int startingCity = 1;
+
+        // Locks
+        /// <summary>
+        /// route array lock
+        /// </summary>
+        private object routeLock = new object();
+
+        private Route[] _routes;
+        /// <summary>
+        /// Routes between cities
+        /// </summary>
+        public Route[] routes
+        {
+            get
+            {
+                lock (routeLock)
+                {
+                    return _routes;
+                }
+            }
+
+            init
+            {
+                lock(routeLock)
+                {
+                    _routes = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// All Possible Trips
+        /// </summary>
+        Permutations<int> trips;
+
+        /// <summary>
+        /// Current best result
+        /// </summary>
+        public BestResult bestResult;
+
+        /// <summary>
+        /// Position in the permutation array
+        /// </summary>
+        private ulong pos = 0;
+
+        /// <summary>
+        /// Trip Enumerator
+        /// </summary>
+        IEnumerator<IReadOnlyList<int>> tripEnumerator;
+
+        /// <summary>
+        /// Constructor, initializes the work manager
+        /// </summary>
+        /// <param name="cities">ints representing the avalible cities</param>
+        /// <param name="routes">Array of routes between the cities</param>
+        /// <param name="startingCity">int of the city to start in</param>
+        public WorkManager(int[] cities, Route[] routes, int startingCity)
+        {
+            // Cities
+            this.cities = cities;
+
+            // Random
+            Random rnd = new Random();
+
+            // All Possible Trips
+            trips = new Permutations<int>(cities);
+
+            // Routes
+            this.routes = routes;
+
+            // Trip Enumerator
+            tripEnumerator = trips.GetEnumerator();
+
+            // Starting City
+            this.startingCity = startingCity;
+
+            // Best Result
+            bestResult = new BestResult(int.MaxValue, null);
+        }
+
+        /// <summary>
+        /// Requests work
+        /// </summary>
+        /// <returns>List of permutations to go through</returns>
+        public IReadOnlyList<int>? getWork()
+        {
+            lock (trips)
+            {
+                if (tripEnumerator.MoveNext()) 
+                {
+                    pos += 1;
+                    return tripEnumerator.Current;
+                }
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Submit a result
+        /// </summary>
+        /// <param name="trip">The list of cities</param>
+        /// <param name="cost">Cost of the route</param>
+        public void submitResult(List<int> trip, int cost)
+        {
+            if(cost < bestResult.cost)
+            {
+                bestResult.route = trip.ToArray();
+                bestResult.cost = cost;
+                Console.WriteLine("New " + bestResult);
+            }
+        }
+    }
+}
